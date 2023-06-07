c72cb885ccb717a36b15499aa4390340f05be8e7
Andrew Dupuis <apd47@case.edu>
2023-06-07 16:10:55 -0400

    Testing docstrings

--- gadgets/AcquisitionPassthroughChannelGadget.cpp
+++ gadgets/AcquisitionPassthroughChannelGadget.cpp
@@ -1,12 +1,18 @@
 /*********************************************************************
- * @file  AcquisitionPassthroughChannelGadget.cpp
+ * \file  AcquisitionPassthroughChannelGadget.cpp
  * 
- * @brief Implementation of the class AcquisitionPassthroughChannelGadget.
+ * \brief Implementation of the class AcquisitionPassthroughChannelGadget.
  *********************************************************************/
 
 #include "AcquisitionPassthroughChannelGadget.h"
 
 namespace Gadgetron{
+
+
+/********************************************//**
+ *  Accepts incoming acquisition data from an InputChannel and immediately pushes it out via an OutputChannel
+ *  with no changes to the data. 
+ ***********************************************/
   void Gadgetron::AcquisitionPassthroughChannelGadget::process(Core::InputChannel<Core::Acquisition>& in, Core::OutputChannel& out) {
       for (auto acquisition : in) {
         // Get the header, image data, and trajectory for this acquisition
